561d6b74b0650dc9c24ee181c4488d45042c3f26
Rajan Poudel <40542238@live.napier.ac.uk>
2023-02-27 09:48:21 +0000

    extracting connect and disconnect func

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -6,57 +6,78 @@ public class App
 {
     public static void main(String[] args)
     {
-        try
-        {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
+            // Create new Application
+            App a = new App();
 
-        // Connection to the database
+            // Connect to database
+            a.connect();
+
+            // Disconnect from database
+            a.disconnect();}
+
+        /**
+         * Connection to MySQL database.
+         */
         Connection con = null;
-        int retries = 100;
-        for (int i = 0; i < retries; ++i)
+
+        /**
+         * Connect to the MySQL database.
+         */
+        public void connect()
         {
-            System.out.println("Connecting to database...");
             try
             {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
-                break;
+                // Load Database driver
+                Class.forName("com.mysql.cj.jdbc.Driver");
             }
-            catch (SQLException sqle)
+            catch (ClassNotFoundException e)
             {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
+                System.out.println("Could not load SQL driver");
+                System.exit(-1);
             }
-            catch (InterruptedException ie)
+
+            int retries = 10;
+            for (int i = 0; i < retries; ++i)
             {
-                System.out.println("Thread interrupted? Should not happen.");
+                System.out.println("Connecting to database...");
+                try
+                {
+                    // Wait a bit for db to start
+                    Thread.sleep(30000);
+                    // Connect to database
+                    con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+                    System.out.println("Successfully connected");
+                    break;
+                }
+                catch (SQLException sqle)
+                {
+                    System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                    System.out.println(sqle.getMessage());
+                }
+                catch (InterruptedException ie)
+                {
+                    System.out.println("Thread interrupted? Should not happen.");
+                }
             }
         }
 
-        if (con != null)
+        /**
+         * Disconnect from the MySQL database.
+         */
+        public void disconnect()
         {
-            try
+            if (con != null)
             {
-                // Close connection
-                con.close();
-            }
-            catch (Exception e)
-            {
-                System.out.println("Error closing connection to database");
+                try
+                {
+                    // Close connection
+                    con.close();
+                }
+                catch (Exception e)
+                {
+                    System.out.println("Error closing connection to database");
+                }
             }
         }
-    }
-}
+
+    };
